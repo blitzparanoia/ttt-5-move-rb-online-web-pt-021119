98c942bc23db2abae532b982936580c3d0c71a26
Irene Saez <bluemilk44@gmail.com>
2019-02-11 23:22:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,4 +12,12 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player = "X")
+  board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+ 
+update_array_at_with(board, 0, "X")
+# The element at index 0 of array 'board' is set to the value "X"
+board #=> ["X", " ", " "]
 end
