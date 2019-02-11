565eb7725d4be91f357ffd141c2c16adf0e96ff7
Irene Saez <bluemilk44@gmail.com>
2019-02-11 23:17:57 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,6 +7,6 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(move)
-  move = gets.chomp.to_i
+def input_to_index(user_input)
+  user_input
 end
